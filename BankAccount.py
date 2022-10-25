fdf206b6387a53a16683506cc43dd6f87d768382
Andrew Pham <andrew.pham@shopify.com>
2022-10-24 21:36:31 -0400

    Use an expression in a formatted string literal to format the balance into a currency-like string

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,3 @@
-def returns_home():
-    return "hello"
-
-
 class BankAccount:
     def __init__(self, full_name, account_number, balance):
         self.full_name = full_name
@@ -10,27 +6,30 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += float(amount)
-        print(f"Amount deposited: ${amount}. New Balance: ${round(self.balance, 2)}")
+        print(f"Amount deposited: ${amount}. New Balance: ${self.balance:,.2f}")
 
     def withdraw(self, amount):
         self.balance -= float(amount)
-        print(f"Amount withdrawn: ${amount}. New Balance: ${round(self.balance, 2)}")
+        print(f"Amount withdrawn: ${amount}. New Balance: ${self.balance:,.2f}")
 
     def get_balance(self):
-        print(f"Your current balance is: ${self.balance}")
+        print(f"Your current balance is: ${self.balance:,.2f}")
         return self.balance
 
     def add_interest(self):
         interest = self.balance * 0.00083
         self.balance += round(interest, 2)
-        print(f"Monthly interest added. New balance: ${self.balance}")
+        print(f"Monthly interest added. New balance: ${self.balance:,.2f}")
 
     def print_statement(self):
         account_number_str = str(self.account_number)
 
         print(
-            f"{self.full_name}\nAccount No.: ****{account_number_str[4:]}\nBalance: ${self.balance}"
+            f"{self.full_name}\nAccount No.: ****{account_number_str[4:]}\nBalance: ${self.balance:,.2f}"
         )
 
 
-andrew_bank = BankAccount("Andrew Pham", 12345678, 100)
+andrew_bank = BankAccount("Andrew Pham", 12345678, 100000)
+andrew_bank.print_statement()
+andrew_bank.get_balance()
+andrew_bank.deposit(1234.126)
