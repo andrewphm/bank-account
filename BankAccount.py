4f19343a16a0c082d58245a2a3355a714d926484
Andrew Pham <andrew.pham@shopify.com>
2022-10-24 21:24:36 -0400

    add methods to add interest and print statement. Add rounding format

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,7 @@
-from symbol import test_nocond
+def returns_home():
+    return "hello"
 
 
-# test
 class BankAccount:
     def __init__(self, full_name, account_number, balance):
         self.full_name = full_name
@@ -9,13 +9,28 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
-        print(f"Amount deposited: ${amount}. New Balance: ${self.balance}")
+        self.balance += float(amount)
+        print(f"Amount deposited: ${amount}. New Balance: ${round(self.balance, 2)}")
 
     def withdraw(self, amount):
-        self.balance -= amount
-        print(f"Amount withdrawn: ${amount}. New Balance: ${self.balance}")
+        self.balance -= float(amount)
+        print(f"Amount withdrawn: ${amount}. New Balance: ${round(self.balance, 2)}")
 
     def get_balance(self):
         print(f"Your current balance is: ${self.balance}")
         return self.balance
+
+    def add_interest(self):
+        interest = self.balance * 0.00083
+        self.balance += round(interest, 2)
+        print(f"Monthly interest added. New balance: ${self.balance}")
+
+    def print_statement(self):
+        account_number_str = str(self.account_number)
+
+        print(
+            f"{self.full_name}\nAccount No.: ****{account_number_str[4:]}\nBalance: ${self.balance}"
+        )
+
+
+andrew_bank = BankAccount("Andrew Pham", 12345678, 100)
